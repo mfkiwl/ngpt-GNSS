3a30ea67de6e4588b73655c392a05595f2da9a8c
xanthos <xanthos@my.home>
2016-01-12 01:36:48 +0200

    added argparse to antexview

--- bin/antex-view.py
+++ bin/antex-view.py
@@ -4,6 +4,8 @@ from mpl_toolkits.mplot3d import axes3d
 from matplotlib import cm
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
+from argparse import RawTextHelpFormatter
 import sys
 
 plot_options = {'wired': False,  ## plot as wire-frame
@@ -47,9 +49,43 @@ def get_azi_axis(buf):
     # return np.arange(azi1, azi2, dazi)
     return azi1, azi2, dazi
 
+parser = argparse.ArgumentParser(
+    formatter_class = RawTextHelpFormatter,
+    description     = ''
+    'Utility to plot GNSS antenna Phase Center Variation correction\n'
+    'grid(s), as extracted from the ngpt atxtr program.'
+    )
+
+parser.add_argument('-f', '--file',
+    action   = 'store',
+    required = True,
+    help     = ''
+    'The input file containing the PCV corrections.\n'
+    'The format of this file is strict; see the\n'
+    '\"atx.grd.example\" file for details. Note that\n'
+    'more than one antennas PCVs can be plotted in one\n'
+    'run.',
+    metavar  = 'PVC_GRID',
+    dest     = 'pcv_grid_file'
+    )
+
+## TODO implement this
+parser.add_argument('-i', '--altitude',
+    action = 'store_true',
+    help   = ''
+    'Instead of plotting the zenith distance (versus\n'
+    'azimouth), plot the altitude. Note that the zenith\n'
+    'distance is the complement of altitude (i.e.\n'
+    '90 - altitude).',
+    dest   = 'plot_altitude'
+    )
+
+##  Parse command line arguments
+args = parser.parse_args()
+
 FIG_NR = 0 
 new_antenna = True
-with open('atx.grd', 'r') as fin:
+with open(args.pcv_grig_file, 'r') as fin:
     while new_antenna:
         try:
             antenna = get_antenna_model(fin)
@@ -75,16 +111,21 @@ with open('atx.grd', 'r') as fin:
             idx += 1
             print 'read',idx,'/',max_lines, '(FIG=', FIG_NR,')'
 
-##  Setup plot env
+        ##  Setup plot env
+        ## -------------------------------------------------------------------
         fig = plt.figure(FIG_NR)
         fig.suptitle(antenna, fontsize=14, fontweight='bold')
-# ax  = fig.add_subplot(111, projection='3d')
+        # ax  = fig.add_subplot(111, projection='3d')
         ax = fig.gca(projection='3d')
-        ax.set_xlabel('Zenith Distance ($^\circ$)')
+        if args.plot_altitude:
+            ax.set_xlabel('Altitude ($^\circ$)')
+        else:
+            ax.set_xlabel('Zenith Distance ($^\circ$)')
         ax.set_ylabel('Azimouth ($^\circ$)')
         ax.set_zlabel('Pcv (mm)')
 
-##  Make a (const) zero surface plot
+        ##  Make a (const) zero surface plot
+        ##  ------------------------------------------------------------------
         if plot_options['wired'] == True:
             X, Y = np.meshgrid(np.arange(zen1-2*dzen, zen2+2*dzen, dzen),
                                np.arange(azi1-2*dazi, azi2+2*dazi, dazi) )
@@ -96,7 +137,8 @@ with open('atx.grd', 'r') as fin:
                                    linewidth=0,
                                    antialiased=False)
 
-##  Plot the pcv values
+        ##  Plot the pcv values
+        ##  ------------------------------------------------------------------
         zen  = np.arange(zen1, zen2, dzen)
         azi  = np.arange(azi1, azi2, dazi)
         X, Y = np.meshgrid(zen, azi)
@@ -107,7 +149,7 @@ with open('atx.grd', 'r') as fin:
                 Z[i,j] = pcv[k]
                 k += 1
         Z = np.transpose(Z)
-## Wired-plot
+        ## Wired-plot
         if plot_options['wired'] == True:
             ax.plot_wireframe(X, Y, Z, rstride=5, cstride=5)
 
@@ -117,7 +159,7 @@ with open('atx.grd', 'r') as fin:
             #cset = ax.contourf(X, Y, Z, zdir='x', offset= -15,         cmap=cm.coolwarm)
             #cset = ax.contourf(X, Y, Z, zdir='y', offset=340,         cmap=cm.coolwarm)
 
-##  Re-scale
+        ##  Re-scale
         ax.set_xlim(0, 90.0)  ## zenith
         ax.set_ylim(0, 360.0) ## azimuth
         ax.set_zlim(2*min(pcv), max(pcv)+1)
