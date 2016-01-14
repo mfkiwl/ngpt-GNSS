b5b824e580c3377ce41c71a2542a8000a13770e3
xanthos <xanthos@mail.ntua.gr>
2016-01-14 12:37:12 +0200

    antex-view seems to work fine.

--- bin/antex-view.py
+++ bin/antex-view.py
@@ -2,14 +2,44 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import argparse
-import sys, re
+import argparse, sys, re, itertools
+
 from mpl_toolkits.mplot3d import axes3d
 from matplotlib import cm
 from argparse import RawTextHelpFormatter
 
 plot_options = {'wired': False,  ## plot as wire-frame
-                'surfc': True }  ## normal, surface plot
+                'surfc': False,  ## normal, surface plot
+                'contr': False   ## contour plot
+                }  
+
+def get_pcv_vals(buf, zen1, zen2, dzen, azi1, azi2, dazi):
+    """ Read the pcv grid values off from an atxtr data file.
+    """
+    pcv_vals = []
+    num_of_rows = int( (zen2 - zen1)/dzen )
+    num_of_cols = 0
+    if dazi != 0.0: num_of_cols = int( (azi2 - azi1)/dazi )
+    lines_to_read = num_of_rows
+    if dazi != 0.0: lines_to_read *= num_of_cols
+    min_pcv = sys.float_info.max
+    max_pcv = sys.float_info.min
+    counter = 0
+    if not num_of_cols: num_of_cols = 1
+    for z in range(0, num_of_rows):
+        pcv_vals.append([])
+        for a in range(0, num_of_cols):
+            line = buf.readline()
+            counter += 1
+            if not line:
+                print >> sys.stderr, '!! ERROR. Could not read pcv vals. !!'
+                sys.exit(1)
+            val = float(line)
+            if val < min_pcv : min_pcv = val
+            if val > max_pcv : max_pcv = val
+            pcv_vals[(len(pcv_vals)-1)].append(val)
+    if args.debug_mode: print '## Read off', counter,'pcv lines'
+    return min_pcv, max_pcv, pcv_vals
 
 def get_antenna_model(buf):
     """ Extract the antenna model from an input stream.
@@ -59,18 +89,7 @@ def get_azi_axis(buf):
 
 def make_2d_plot(antenna, buf, fig_nr, zen1, zen2, dzen):
     pcv = []
-    max_lines = int((zen2 - zen1)/dzen)
-    idx       = 0
-    while idx < max_lines:
-        line = buf.readline()
-        l    = line.split()
-        if len(l) != 1:
-            print >> sys.stderr, '!! ERROR. Invalid line PCV line !!',
-            print >> sys.stderr, '!!        [' + line.strip() + '] !!'
-            raise RuntimeError
-        pcv.append(float(l[0]))
-        idx += 1
-        ##print 'read',idx,'/',max_lines, '(FIG=', fig_nr,')'
+    min_pcv, max_pcv, pcv = get_pcv_vals(buf, zen1, zen2, dzen, 0, 0, 0)
     ##  Setup plot env
     ## -------------------------------------------------------------------
     fig = plt.figure(fig_nr)
@@ -82,9 +101,9 @@ def make_2d_plot(antenna, buf, fig_nr, zen1, zen2, dzen):
         ax.set_xlabel('Zenith Distance ($^\circ$)')
     ax.set_ylabel('Pcv (mm)') 
     X = np.arange(zen1, zen2, dzen)
-    Y = np.array(pcv)
-    ax.set_xlim(zen1-1, zen2+1)  ## zenith
-    ax.set_ylim(min(pcv)-1,max(pcv)+1) ## pcv
+    Y = np.array(itertools.chain(*pcv))
+    ax.set_xlim(zen1, zen2) if zen2 > zen1 else ax.set_xlim(zen2, zen1)## zenith
+    ax.set_ylim(min_pcv-1,max_pcv+1) ## pcv
     ax.plot(X, Y, 'yo-')
 
     ## Export (if needed)
@@ -92,7 +111,7 @@ def make_2d_plot(antenna, buf, fig_nr, zen1, zen2, dzen):
     if args.save_format is not None:
         fn = (re.sub(r'\s+', '_', antenna) + '.' + args.save_format).lower()
         fig.savefig(fn, dpi=100, bbox_inches='tight', format=args.save_format)
-        print '## Plot exported to:', fn,
+        if args.degub: print '## Plot exported to:', fn,
     return fig
 
 
@@ -101,34 +120,31 @@ def make_3d_plot(antenna, buf, fig_nr):
     ## -----------------------------------------------------------------------
     pcv = []
     zen1, zen2, dzen = get_zen_axis(buf)
+    if args.plot_altitude:
+        zen1, zen2 = 90-zen1, 90-zen2
+        dzen = -1 * dzen
+        if args.debug_mode: print '## Ploting altitude [',zen1,zen2,dzen,')'
     azi1, azi2, dazi = get_azi_axis(buf)
     if azi1 == azi2 or dazi == 0:
+        if args.debug:
+            print '## No azi-dependent pcv; falling back to NOAZI plot.'
         return make_2d_plot(antenna, buf, fig_nr, zen1, zen2, dzen)
-    ## how many lines should i read ?? TODO limits are not drawn so NOT:
-    #max_lines = int((zen2 - zen1)/dzen + 1) * int((azi2 - azi1)/dazi + 1)
-    ## BUT
-    max_lines = int((zen2 - zen1)/dzen) * int((azi2 - azi1)/dazi)
-    idx       = 0
-    while idx < max_lines:
-        line = buf.readline()
-        l    = line.split()
-        if len(l) != 1:
-            print >> sys.stderr, '!! ERROR. Invalid line PCV line !!',
-            print >> sys.stderr, '!!        [' + line.strip() + '] !!'
-            raise RuntimeError
-        pcv.append(float(l[0]))
-        idx += 1
+    min_pcv, max_pcv, pcv = get_pcv_vals(buf, zen1, zen2, dzen, azi1, azi2, dazi)
     ##  Setup plot env
     ## -------------------------------------------------------------------
     fig = plt.figure(fig_nr)
     fig.suptitle(re.sub(r'\s+', ' ', antenna), fontsize=14, fontweight='bold')
-    ax  = fig.gca(projection='3d')
+    if plot_options['wired'] or plot_options['surfc']:
+        ax  = fig.gca(projection='3d')
+    else:
+        ax  = fig.gca()
     if args.plot_altitude:
         ax.set_xlabel('Altitude ($^\circ$)')
     else:
         ax.set_xlabel('Zenith Distance ($^\circ$)')
     ax.set_ylabel('Azimouth ($^\circ$)')
-    ax.set_zlabel('Pcv (mm)') 
+    if plot_options['wired'] or plot_options['surfc']:
+        ax.set_zlabel('Pcv (mm)') 
     ##  Make a (const) zero surface plot
     ##  This does not look good ....
     ##  ------------------------------------------------------------------
@@ -148,33 +164,46 @@ def make_3d_plot(antenna, buf, fig_nr):
     azi  = np.arange(azi1, azi2, dazi)
     X, Y = np.meshgrid(zen, azi)
     Z    = np.zeros((len(zen), len(azi)))
-    k = 0
     for i in range(0, len(zen)):
         for j in range(0, len(azi)):
-            Z[i,j] = pcv[k]
-            k += 1
+            #print '[',i,j,']=',pcv[i][j]
+            Z[i,j] = pcv[i][j]
+            #if args.debug_mode:
+            #    print '\tpcv(%+02.2f, %03.1f) = %+03.1f'%(zen1+i*dzen,
+            #            azi1+j*dazi, Z[i,j])
+    if args.debug_mode:
+        print '## Zen/Alt axis: from %+03.1f to %+03.1f with step %+03.1f'%(zen1, zen2, dzen)
     Z = np.transpose(Z)
     ## Wired-plot
     if plot_options['wired'] == True:
         ax.plot_wireframe(X, Y, Z, rstride=5, cstride=5)
     if plot_options['surfc'] == True:
         ax.plot_surface(X, Y, Z, rstride=5, cstride=5, alpha=0.3)
+    if plot_options['contr'] == True:
+        levels = np.arange(min_pcv-1, max_pcv+1, 1)
+        norm = cm.colors.Normalize(vmax=abs(Z).max(), vmin=-abs(Z).max())
+        cmap = cm.PRGn
+        cs = ax.contourf(X, Y, Z, levels,
+            cmap=cm.get_cmap(cmap, len(levels) - 1),
+            norm=norm)
+        plt.colorbar( cs )
     ## projections/contours
-    cset = ax.contourf(X, Y, Z, zdir='z', offset=2*min(pcv), cmap=cm.coolwarm)
-    #cset = ax.contourf(X, Y, Z, zdir='x', offset= -15,         cmap=cm.coolwarm)
-    #cset = ax.contourf(X, Y, Z, zdir='y', offset=340,         cmap=cm.coolwarm)
+    if plot_options['surfc'] == True:
+        cset = ax.contourf(X, Y, Z, zdir='z', offset=2*min_pcv, cmap=cm.coolwarm)
+        cset = ax.contourf(X, Y, Z, zdir='x', offset=0, cmap=cm.coolwarm)
+        cset = ax.contourf(X, Y, Z, zdir='y', offset=azi2+10, cmap=cm.coolwarm)
     ##  Re-scale
     ## -----------------------------------------------------------------------
-    ax.set_xlim(0, zen2)  ## zenith
-    ax.set_ylim(0, azi2) ## azimuth
-    ax.set_zlim(2*min(pcv), max(pcv)+1)
-
+    if plot_options['surfc'] == True:
+        ax.set_xlim(zen1, zen2) if zen2 > zen1 else ax.set_xlim(zen2, zen1) ## zenith
+        ax.set_ylim(azi1, azi2)  ## azimuth
+        ax.set_zlim(2*min_pcv, max_pcv+1)
     ## Export (if needed)
     ## -------------------------------------------------------------------
     if args.save_format is not None:
         fn = (re.sub(r'\s+', '_', antenna) + '.' + args.save_format).lower()
         fig.savefig(fn, dpi=100, bbox_inches='tight', format=args.save_format)
-        print '## Plot exported to:', fn,
+        if args.debug: print '## Plot exported to:', fn,
     return fig
 
 parser = argparse.ArgumentParser(
@@ -231,7 +260,12 @@ parser.add_argument('-f', '--file',
     default  = sys.stdin
     )
 
-## TODO implement this
+parser.add_argument('--debug',
+    action = 'store_true',
+    help   = 'Enable debug mode.',
+    dest   = 'debug_mode'
+    )
+
 parser.add_argument('-i', '--altitude',
     action = 'store_true',
     help   = ''
@@ -260,6 +294,22 @@ parser.add_argument('-s', '--save-as',
     choices  = ['png', 'pdf', 'eps', 'ps', 'svg']
     )
 
+parser.add_argument('-t', '--plot-style',
+    action   = 'store',
+    default  = 'surfc',
+    help     = ''
+    'Choosing plot style. You can choose between:\n'
+    '\t* \'surfc\' (default) compile a 3D plot,\n'
+    '\t* \'wired\' compile a 3D wired-plot,\n'
+    '\t* \'contr\' compile a 2D contour plot\n'
+    'Note that this is only relevant in azimouth-\n'
+    'dependent pcv grids.',
+    required = False,
+    metavar  = 'PLOT_STYLE',
+    dest     = 'plot_style',
+    choices  = ['surfc', 'wired', 'contr']
+    )
+
 parser.add_argument('-n', '--non-interactive',
     action   = 'store_true',
     help     = ''
@@ -274,6 +324,9 @@ args = parser.parse_args()
 FIG_NR      = 0
 plots       = []
 new_antenna = True
+
+## set the plot style
+plot_options[args.plot_style] = True
 
 with open(args.pcv_grid_file, 'r') if args.pcv_grid_file is not sys.stdin else sys.stdin as fin:
     while new_antenna:
