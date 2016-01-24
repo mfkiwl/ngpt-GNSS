e0636e5cbd79a01830185cb20e69638b4591f68c
xanthos <xanthos@my.home>
2016-01-24 02:30:41 +0200

    debiging dates v2

--- src/datetime_v2.hpp
+++ src/datetime_v2.hpp
@@ -20,6 +20,7 @@ static_assert( 86400L  * 1000000L * 2 < std::numeric_limits<long>::max(),
 class year {
     int y;
 public:
+    typedef int underlying_type;
     explicit constexpr year (int i) noexcept : y(i) {};
     constexpr int as_int() const noexcept { return y; }
 };
@@ -27,13 +28,23 @@ public:
 class month {
     int m;
 public:
+    typedef int underlying_type;
     explicit constexpr month (int i) noexcept : m(i) {};
     constexpr int as_int() const noexcept { return m; }
+};
+
+class day {
+    int d;
+public:
+    typedef int underlying_type;
+    explicit constexpr day(int i) noexcept : d(i) {};
+    constexpr int as_int() const noexcept { return d; }
 };
 
 class day_of_month {
     int d;
 public:
+    typedef int underlying_type;
     explicit constexpr day_of_month(int i) noexcept : d(i) {};
     constexpr int as_int() const noexcept { return d; }
 };
@@ -41,43 +52,93 @@ public:
 class modified_julian_day {
     long m;
 public:
+    typedef long underlying_type;
     explicit constexpr modified_julian_day(long i) noexcept : m(i) {};
+    constexpr long as_long() const noexcept { return m; }
+    constexpr long& assign() noexcept { return m; }
+    constexpr void operator+=(const modified_julian_day& d) noexcept
+    { m += d.m; }
+    constexpr void operator+=(const day& d) noexcept
+    { m += d.as_int(); }
 };
 
 class julian_day {
     long j;
 public:
+    typedef long underlying_type;
     explicit constexpr julian_day(long i) noexcept : j(i) {};
+    constexpr long as_long() const noexcept { return j; }
+    constexpr long& assign() noexcept { return j; }
+    constexpr void operator+=(const julian_day& d) noexcept
+    { j += d.j; }
+    constexpr void operator+=(const day& d) noexcept
+    { j += d.as_int(); }
 };
 
 class seconds {
     long s;
-    static constexpr long max_in_day { 86400L };
 public:
+    static constexpr long max_in_day { 86400L };
+    typedef long underlying_type;
     explicit constexpr seconds(long i=0L) noexcept : s(i) {};
     constexpr void operator+=(const seconds& sc) noexcept { s+=sc.s; }
+    constexpr seconds operator/(const seconds& sc) noexcept
+    { return seconds(s/sc.s); }
     static constexpr bool is_sec_type { true };
+    constexpr bool more_than_day() const noexcept { return s>max_in_day; }
+    constexpr long as_long() const noexcept { return s; }
+    constexpr long& assign() noexcept { return s; }
+    constexpr day to_days() const noexcept {
+        return day(static_cast<int>(s/max_in_day));
+    }
+    constexpr double fractional_days() const noexcept {
+        return static_cast<double>(s)/static_cast<double>(max_in_day);
+    }
 };
 
 class milliseconds {
     long s;
-    static constexpr long max_in_day { 86400L * 1000L };
 public:
+    static constexpr long max_in_day { 86400L * 1000L };
+    typedef long underlying_type;
     explicit constexpr milliseconds(long i=0L) noexcept : s(i) {};
     constexpr explicit operator seconds() const { return seconds(s/1000L); }
     constexpr void operator+=(const milliseconds& ms) noexcept { s+=ms.s; }
+    constexpr milliseconds operator/(const milliseconds& sc) noexcept
+    { return milliseconds(s/sc.s); }
     static constexpr bool is_sec_type { true };
+    constexpr bool more_than_day() const noexcept { return s>max_in_day; }
+    constexpr long as_long() const noexcept { return s; }
+    constexpr long& assign() noexcept { return s; }
+    constexpr day to_days() const noexcept {
+        return day(static_cast<int>(s/max_in_day));
+    }
+    constexpr double fractional_days() const noexcept {
+        return static_cast<double>(s)/static_cast<double>(max_in_day);
+    }
 };
 
 class nanoseconds {
     long s;
-    static constexpr long max_in_day { 86400L * 1000L };
 public:
+    static constexpr long max_in_day { 86400L * 1000L };
+    typedef long underlying_type;
     explicit constexpr nanoseconds(long i=0L) noexcept : s(i) {};
     constexpr explicit operator milliseconds() const { return milliseconds(s/1000L); }
     constexpr explicit operator seconds() const { return seconds(s/1000000L); }
     constexpr void operator+=(const nanoseconds& ns) noexcept { s+=ns.s; }
+    constexpr nanoseconds operator/(const nanoseconds& sc) noexcept
+    { return nanoseconds(s/sc.s); }
     static constexpr bool is_sec_type { true };
+    constexpr bool more_than_day() const noexcept { return s>max_in_day; }
+    constexpr long as_long() const noexcept { return s; }
+    constexpr long& assign() noexcept { return s; }
+    constexpr day to_days() const noexcept {
+        return day(static_cast<int>(s/max_in_day));
+    }
+    constexpr double fractional_days() const noexcept {
+        return static_cast<double>(s)/static_cast<double>(max_in_day);
+    }
 };
 
 template<class S>
@@ -101,9 +162,31 @@ public:
     {}
 
     template<class T>
-    constexpr void add_seconds(T&& t) noexcept
-    { sect_+=(S)t; } 
+    constexpr void add_seconds(T t) noexcept
+    { 
+        //std::cout<<"\nSecs was -> "<< sect_.as_long();
+        sect_ += (S)t;
+        //std::cout<<" now became -> "<<sect_.as_long();
+        if ( sect_.more_than_day() ) this->normalize();
+        return;
+    }
 
+    constexpr void normalize() noexcept {
+        //std::cout<<"\nNORMALIZING!";
+        //std::cout<<"\nMJD="<<mjd_.as_long()<<", secs="<<sect_.as_long();
+        long m = mjd_.as_long();
+        long s = sect_.as_long();
+        long d = S::max_in_day;
+        m += s / d; 
+        s %= d;
+        mjd_.assign() = m;
+        sect_.assign() = s;
+        //std::cout<<"\nMJD="<<mjd_.as_long()<<", secs="<<sect_.as_long();
+        return;
+    }
+
+    constexpr double as_mjd() const noexcept
+    { return static_cast<double>(mjd_.as_long()) + sect_.fractional_days(); }
         
 private:
     modified_julian_day mjd_;  ///< Modified Julian Day
--- test/test_datetime.cpp
+++ test/test_datetime.cpp
@@ -16,7 +16,7 @@ int main()
     //datev2<ngpt::year> d3(ngpt::year(2015), ngpt::month(12), ngpt::day_of_month(30));
  
     // this is fine; nanoseconds to seconds is allowed
-    datev2<ngpt::seconds> d4(ngpt::year(2015), ngpt::month(12),
+    datev2<ngpt::seconds> d24(ngpt::year(2015), ngpt::month(12),
         ngpt::day_of_month(30), ngpt::nanoseconds(100));
 
     // the opposite however id not allowed!
@@ -27,26 +27,54 @@ int main()
     std::cout<<"\nSize of v2 class: "<< sizeof(d2);
 
     std::chrono::steady_clock::time_point begin, end;
-    double mjd;
+    double mjd1=0, mjd2;
 
     begin = std::chrono::steady_clock::now();
     for (int i=0; i<86400 * 2.5; ++i) {
         d1.add_seconds( 1.0e0 );
-        mjd = d1.as_mjd();
+        mjd1 = d1.as_mjd();
     }
     end = std::chrono::steady_clock::now();
 
     std::cout << "\nAdding two days in v1: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() <<std::endl;
-    std::cout << "\nmjd= " << mjd;
+    std::cout << "\nmjd= " << mjd1;
     
+    // if no user input, the noexcept may bias the computation!
+    //long secs;
+    //std::cout<<"\nSeconds to add: ";
+    //std::cin >> secs;
+
     begin = std::chrono::steady_clock::now();
     for (int i=0; i<86400 * 2.5; ++i) {
         d2.add_seconds( ngpt::seconds(1L) );
-        //mjd = dd1.as_mjd();
+        mjd2 = d2.as_mjd();
     }
     end = std::chrono::steady_clock::now();
     std::cout << "\nAdding two days in v2: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() <<std::endl;
-    std::cout << "\nmjd= " << mjd;
+    std::cout << "\nmjd= " << mjd2;
+    std::cout<<"\nDifference (in days) = " << mjd1 - mjd2 << " = " << (mjd1 - mjd2)*86400.0 << " seconds";
+  
+    datev2<ngpt::milliseconds> d3(ngpt::year(2015), ngpt::month(12), ngpt::day_of_month(30));
+    begin = std::chrono::steady_clock::now();
+    for (int i=0; i<86400 * 2.5; ++i) {
+        d3.add_seconds( ngpt::milliseconds(1000L) );
+        mjd2 = d3.as_mjd();
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << "\nAdding two days in v2: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() <<std::endl;
+    std::cout << "\nmjd= " << mjd2;
+    std::cout<<"\nDifference (in days) = " << mjd1 - mjd2 << " = " << (mjd1 - mjd2)*86400.0 << " seconds";
+   
+    datev2<ngpt::nanoseconds> d4(ngpt::year(2015), ngpt::month(12), ngpt::day_of_month(30));
+    begin = std::chrono::steady_clock::now();
+    for (int i=0; i<86400 * 2.5; ++i) {
+        d4.add_seconds( ngpt::nanoseconds(1000000L) );
+        mjd2 = d4.as_mjd();
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << "\nAdding two days in v2: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() <<std::endl;
+    std::cout << "\nmjd= " << mjd2;
+    std::cout<<"\nDifference (in days) = " << mjd1 - mjd2 << " = " << (mjd1 - mjd2)*86400.0 << " seconds";
 
     std::cout << "\n";
     return 0;   
